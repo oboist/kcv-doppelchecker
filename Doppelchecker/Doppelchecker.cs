fccc98dd410ea6c4bdf671b71fc53a63db8b9207
marchrock <m4rchrock@gmail.com>
2016-04-03 12:14:05 +0900

    Initial commit

---
+++ Doppelchecker/Doppelchecker.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.Composition;
+using Grabacr07.KanColleViewer.Composition;
+
+namespace Doppelchecker
+{
+    [ExportMetadata("Title", "Doppelchecker")]
+    [ExportMetadata("Description", "艦娘の所属状況を表示します。")]
+    [ExportMetadata("Version", "0.1.0")]
+    [ExportMetadata("Author", "@hgzr")]
+    [ExportMetadata("Guid", "E2FBF552-0797-464B-BB01-B39A6571C4F9")]
+    [Export(typeof(IPlugin))]
+    public class Doppelchecker : IPlugin
+    {
+        public void Initialize()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
